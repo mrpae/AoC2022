f199b4765ef058bdabb5f083d43310f47b140eee
Markus Rene Pae <markus.pae@estcube.eu>
2022-12-05 19:02:59 +0200

    Dec 5 solutions added + modified README

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # AoC2022
+This project contains all my working Python scripts for **Advent of Code 2022**.
 
-This project contains all my working Python scripts for Advent of Code 2022.
+
+## Table of contents
+
+* dec1: Calorie Counting
+* dec2: Rock Paper Scissors
+* dec3: Rucksack Reorganization
+* dec4: Camp Cleanup
+* dec5: Supply Stacks
+
+## Find me on social media
+[![Linkedin](https://i.stack.imgur.com/gVE0j.png) LinkedIn](https://www.linkedin.com/in/markusrenepae/)
+[![GitHub](https://i.stack.imgur.com/tskMh.png) GitHub](https://github.com/mrpae/)
---
+++ dec5/prob2.py
@@ -0,0 +1,62 @@
+####################################
+#####                          #####
+#####    Data preprocessing    #####
+#####                          #####
+####################################
+
+f = open("input1.txt", "r")
+
+# Since the stacks are relatively simple, then it's much easier to
+# define the stacks as a dictonary, rather than to read the information
+# from the input file.
+
+stacks = {1: ["V", "C", "D", "R", "Z", "G", "B", "W"],
+          2: ["G", "W", "F", "C", "B", "S", "T", "V"],
+          3: ["C", "B", "S", "N", "W"],
+          4: ["Q", "G", "M", "N", "J", "V", "C", "P"],
+          5: ["T", "S", "L", "F", "D", "H", "B"],
+          6: ["J", "V", "T", "W", "M", "N"],
+          7: ["P", "F", "L", "C", "S", "T", "G"],
+          8: ["B", "D", "Z"],
+          9: ["M", "N", "Z", "W"]}
+
+lines = []
+
+for line in f:
+    lines.append(line.strip())
+
+commands = lines[10:]    # this list contains all the necessary commands
+
+f.close()
+
+####################################
+#####                          #####
+#####   Necessary functions    #####
+#####                          #####
+####################################
+
+
+def move(src, dest, quant):
+    global stacks
+    values = stacks[src][-quant:]
+    stacks[src] = stacks[src][:-quant]
+    stacks[dest].extend(values)
+
+
+def executecommand(command):
+    data = command.split(" ")[1::2]
+    move(int(data[1]), int(data[2]), int(data[0]))
+
+
+def printoutput():
+    global stacks
+    output = ""
+    for i in range(1,10):
+        output += stacks[i][-1]
+    print(output)
+
+
+for command in commands:
+    executecommand(command)
+
+printoutput()
